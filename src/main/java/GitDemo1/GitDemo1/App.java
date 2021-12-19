29760fe9063749232addde05169171ba61f169e5
bhaktilata Dash <bhaktilata.dash@enhancesys.com>
2021-12-19 21:08:47 +0530

    added firstproject

---
+++ src/main/java/GitDemo1/GitDemo1/App.java
@@ -0,0 +1,20 @@
+package GitDemo1.GitDemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void adddata(){
+    	int a=5;
+    	int b=6;
+    	int c=a+b;
+    	System.out.println(a+b);
+    	
+    }
+}
